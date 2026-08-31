1cdec9165401d3f9a94f1e02cbfb3063816a6455
IgnacioApuy <i.apuy.1@estudiantec.cr>
2026-08-31 12:42:05 -0600

    feat: Agregar Program.cs con main class

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using Proyecto_MonopoTEC.Clases;
+
+namespace Proyecto_MonopoTEC
+{
+        internal class Program
+        {
+                static void Main(string[] args)
+                {
+                        ClaseTest test = new ClaseTest();
+                        test.Test();
+                }
+        }
+}
