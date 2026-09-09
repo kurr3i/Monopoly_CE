8c711ce9157dfaf7080e9d9a76a225122df77332
IgnacioApuy <i.apuy.1@estudiantec.cr>
2026-09-08 20:30:48 -0600

    feat: prueba la comuncacion lado a lado

--- Server/Motor/Juego.cs
+++ Server/Motor/Juego.cs
@@ -1,198 +1,250 @@
 ﻿using System;
+using Proyecto_MonopoTEC.Compartido;
+using Proyecto_MonopoTEC.Server.Red;
+using Proyecto_MonopoTEC.Server.Modelo;
+using Proyecto_MonopoTEC.Server.Estructuras;
+using Proyecto_MonopoTEC.Server.Hardware;
 
-/// <summary>
-/// Representa el juego.
-/// </summary>
-public class Juego
+namespace Proyecto_MonopoTEC.Server.Motor
 {
-    private Tablero _tableroJuego;
-    private ManejadorAcciones _manejadorAcciones;
-
-    private Jugador jugador1;
-    private Jugador jugador2;
-    private Jugador jugador3;
-    private Jugador jugador4;
-    private Jugador jugadorActual;
-     
-
-    private ColaCircular ColaTurnos;
-    private int Turno;
-
-    private Dado dado;
 
     /// <summary>
-    /// Inicializa una nueva instancia de la clase Juego.
+    /// Representa el juego.
     /// </summary>
-    /// <param name="puertoArduino">Nombre del puerto serial utilizado para comunicarse con el Arduino.</param>
-    public Juego(string puertoArduino)
+    public class Juego
     {
-        _tableroJuego = new Tablero();
-        _tableroJuego.Inicializar();
+        private Tablero _tableroJuego;
+        private ManejadorAcciones _manejadorAcciones;
 
-        _manejadorAcciones = new ManejadorAcciones(puertoArduino, _tableroJuego);
+        private Servidor _server;
+        private RFIDDriver _driver;
 
-        jugador1 = InicializarJugador(123, "Josué", _tableroJuego.Head.Data);
-        jugador2 = InicializarJugador(456, "Joshua", _tableroJuego.Head.Data);
-        jugador3 = InicializarJugador(789, "Kevin", _tableroJuego.Head.Data);
-        jugador4 = InicializarJugador(101, "Ignacio", _tableroJuego.Head.Data);
+        private Jugador jugador1;
+        private Jugador jugador2;
+        private Jugador jugador3;
+        private Jugador jugador4;
+        private Jugador jugadorActual;
 
-        ColaTurnos = InicializarTurnos(jugador1, jugador2, jugador3, jugador4);
-        Turno = 0;
 
-        jugadorActual = ColaTurnos.Peek();
+        private ColaCircular ColaTurnos;
+        private int Turno;
 
-        dado = new Dado();
-    }
+        private Dado dado;
 
-    /// <summary>
-    /// Método empleado para inicializar un jugador. 
-    /// </summary>
-    /// <returns>Retorna el objeto jugador inicializado.</returns>
-    private Jugador InicializarJugador(int id, string nombre, Casilla posicion) // Nota: Este método posteriormente se debe modificar para que se inicialice el jugador con los ingresos del cliente y el arduino.
-    {
-        return new Jugador(id, nombre, posicion);
-    }
-
-    /// <summary>
-    /// Método empleado para inicializar la cola de turnos. 
-    /// </summary>
-    /// <returns>Retorna la cola de turnos inicializada.</returns>
-    private ColaCircular InicializarTurnos(Jugador jugador1, Jugador jugador2, Jugador jugador3, Jugador jugador4)
-    {
-        ColaCircular cola = new ColaCircular();
-
-        cola.Enqueue(jugador1);
-        cola.Enqueue(jugador2);
-        cola.Enqueue(jugador3);
-        cola.Enqueue(jugador4);
-
-        return cola;
-    }
-
-    private AccionCasilla PrepararTurno()
-    {
-        bool sigueEnTurno = true;
-        while (sigueEnTurno)
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase Juego.
+        /// </summary>
+        /// <param name="puertoArduino">Nombre del puerto serial utilizado para comunicarse con el Arduino.</param>
+        public Juego(Servidor server, RFIDDriver driver)
         {
-            if (jugadorActual.EnCarcel)
+            _server = server;
+            _driver = driver;
+
+            _tableroJuego = new Tablero();
+            _tableroJuego.Inicializar();
+
+            _manejadorAcciones = new ManejadorAcciones(driver, _tableroJuego);
+
+            dado = new Dado();
+        }
+
+        /// <summary>
+        /// Método empleado para inicializar un jugador. 
+        /// </summary>
+        /// <returns>Retorna el objeto jugador inicializado.</returns>
+        private Jugador InicializarJugador(int id, string UID, string nombre, Casilla posicion) // Nota: Este método posteriormente se debe modificar para que se inicialice el jugador con los ingresos del cliente y el arduino.
+        {
+            return new Jugador(id, UID, nombre, posicion);
+        }
+
+
+
+        public bool AutenticarJugador(int id, string nombre)
+        {
+            if (id == 0)
             {
-                Console.WriteLine("El jugador está en la carcel");
-                jugadorActual.ReducirCondena();
-                if (jugadorActual.TurnosCarcel == 0)
+                string UID = _driver.ReadUID("AUTH_0");
+                jugador1 = InicializarJugador(id, UID, nombre, _tableroJuego.Head.Data);
+            }
+
+            return true;
+        }
+
+
+        public void PruebaConexion(Mensaje mensaje)
+        {
+            if (mensaje.Comando == Protocolo.Prueba)
+            {
+                Console.WriteLine("[Juego] Recibido: " + mensaje.Contenido);
+
+
+                _server.EnviarMensaje(new Mensaje(Protocolo.Prueba,
+                new
                 {
-                    jugadorActual.SalirCarcel();
+                    prueba1 = "prueba1",
+                    prueba2 = "prueba2"
                 }
-                else
+                ));
+
+
+            }
+        }
+
+
+
+        /// <summary>
+        /// Método empleado para inicializar la cola de turnos. 
+        /// </summary>
+        /// <returns>Retorna la cola de turnos inicializada.</returns>
+        private ColaCircular InicializarTurnos(Jugador jugador1, Jugador jugador2, Jugador jugador3, Jugador jugador4)
+        {
+            ColaCircular cola = new ColaCircular();
+
+            cola.Enqueue(jugador1);
+            cola.Enqueue(jugador2);
+            cola.Enqueue(jugador3);
+            cola.Enqueue(jugador4);
+
+            return cola;
+        }
+
+        private AccionCasilla PrepararTurno()
+        {
+            bool sigueEnTurno = true;
+            while (sigueEnTurno)
+            {
+                if (jugadorActual.EnCarcel)
                 {
-                    Console.WriteLine($"Quedan {jugadorActual.TurnosCarcel} turnos en carcel");
+                    Console.WriteLine("El jugador está en la carcel");
+                    jugadorActual.ReducirCondena();
+                    if (jugadorActual.TurnosCarcel == 0)
+                    {
+                        jugadorActual.SalirCarcel();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Quedan {jugadorActual.TurnosCarcel} turnos en carcel");
+                    }
+                    return AccionCasilla.SinAccion;
                 }
-                return AccionCasilla.SinAccion;
+
+                else if (jugadorActual.TurnosPerdidos != 0)
+                {
+                    jugadorActual.ReducirTurnoPerdido();
+                    return AccionCasilla.SinAccion;
+                }
+
+                Console.Clear();
+                Console.WriteLine("Elige una opción:\n1. Lanzar el dado\n2. Vender propiedad\n3. Ver propiedades");
+                string opcion = ValidarOpcionJugador(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case "1":
+                        sigueEnTurno = false;
+                        break;
+                    case "2":
+                        _manejadorAcciones.AccionVenderPropiedad(jugadorActual, Turno);
+                        Console.WriteLine("Presiona Enter para continuar al siguiente turno...");
+                        Console.ReadLine(); // Esperar a que el jugador presione Enter antes de continuar
+
+                        break;
+                    case "3":
+                        Console.WriteLine("Sus propiedades son:");
+                        jugadorActual.PropiedadesAdquiridas.Display();
+                        Console.WriteLine("Presiona Enter para continuar al siguiente turno...");
+                        Console.ReadLine(); // Esperar a que el jugador presione Enter antes de continuar
+
+                        break;
+                }
             }
 
-            else if (jugadorActual.TurnosPerdidos != 0)
+            int resultadoDado1 = dado.Lanzar();
+            int resultadoDado2 = dado.Lanzar();
+
+            Console.WriteLine("El jugador " + jugadorActual.Nombre + " ha lanzado el dado y obtuvo: " + resultadoDado1 + " y " + resultadoDado2);
+            Console.WriteLine("Eso suma: " + (resultadoDado1 + resultadoDado2));
+
+            bool pasoPorSalida;
+            Casilla casillaJugadorActual = _tableroJuego.AvanzarJugador(jugadorActual, resultadoDado1 + resultadoDado2, out pasoPorSalida);
+            Console.WriteLine("El jugador " + jugadorActual.Nombre + " se ha movido a la casilla: " + casillaJugadorActual.Nombre);
+            if (pasoPorSalida)
             {
-                jugadorActual.ReducirTurnoPerdido();
-                return AccionCasilla.SinAccion;
+                _manejadorAcciones.DarPremio(jugadorActual, Turno);
             }
 
-            Console.Clear();
-            Console.WriteLine("Elige una opción:\n1. Lanzar el dado\n2. Vender propiedad\n3. Ver propiedades");
-            string opcion = ValidarOpcionJugador(Console.ReadLine());
+            Console.WriteLine("Presiona Enter para continuar...");
+            Console.ReadLine(); // Esperar a que el jugador presione Enter antes de continuar
 
-            switch (opcion)
-            {
-                case "1":
-                    sigueEnTurno = false; 
-                    break;
-                case "2":
-                    _manejadorAcciones.AccionVenderPropiedad(jugadorActual,Turno);
-                    Console.WriteLine("Presiona Enter para continuar al siguiente turno...");
-                    Console.ReadLine(); // Esperar a que el jugador presione Enter antes de continuar
+            return casillaJugadorActual.DevolverAccion(jugadorActual);
 
-                    break;
-                case "3":
-                    Console.WriteLine("Sus propiedades son:");
-                    jugadorActual.PropiedadesAdquiridas.Display();
-                    Console.WriteLine("Presiona Enter para continuar al siguiente turno...");
-                    Console.ReadLine(); // Esperar a que el jugador presione Enter antes de continuar
-
-                    break;
-            }
         }
 
-        int resultadoDado1 = dado.Lanzar();
-        int resultadoDado2 = dado.Lanzar();
 
-        Console.WriteLine("El jugador " + jugadorActual.Nombre + " ha lanzado el dado y obtuvo: " + resultadoDado1 + " y " + resultadoDado2);
-        Console.WriteLine("Eso suma: " + (resultadoDado1 + resultadoDado2));
-
-        bool pasoPorSalida;
-        Casilla casillaJugadorActual = _tableroJuego.AvanzarJugador(jugadorActual, resultadoDado1 + resultadoDado2, out pasoPorSalida);
-        Console.WriteLine("El jugador " + jugadorActual.Nombre + " se ha movido a la casilla: " + casillaJugadorActual.Nombre);
-        if (pasoPorSalida)
+        // Falta comentar esto.
+        private string ValidarOpcionJugador(string opcion)
         {
-            _manejadorAcciones.DarPremio(jugadorActual, Turno); 
+            while (opcion != "1" && opcion != "2" && opcion != "3")
+            {
+                Console.WriteLine("Opción inválida. Por favor, elige una opción válida.");
+                opcion = Console.ReadLine();
+            }
+            return opcion;
         }
 
-        Console.WriteLine("Presiona Enter para continuar...");
-        Console.ReadLine(); // Esperar a que el jugador presione Enter antes de continuar
-
-        return casillaJugadorActual.DevolverAccion(jugadorActual);
-
-    }
 
 
-    // Falta comentar esto.
-    private string ValidarOpcionJugador(string opcion)
-    {
-        while (opcion != "1" && opcion != "2" && opcion != "3")
+        public void IniciarJuego()
         {
-            Console.WriteLine("Opción inválida. Por favor, elige una opción válida.");
-            opcion = Console.ReadLine();
+            // Me falta añadir el lobby antes de comenzar la partida
         }
-        return opcion;
-    }
 
-    
 
-    // Falta comentar esto.
-    public void IniciarJuego()
-    {
-        Console.WriteLine("El juego ha comenzado. El jugador actual es: " + jugadorActual.Nombre);
 
-        while (true) // Bucle infinito para el juego
+        // Falta comentar esto.
+        public void IniciarPartida()
         {
-            AccionCasilla accion = PrepararTurno();
-            Console.WriteLine(accion);
-
-            bool sigueEnJuego = _manejadorAcciones.EjecutarAccion(accion, jugadorActual, Turno);
-            if (sigueEnJuego)
-            {
-                ColaTurnos.Advance();
-            }
-            else
-            {
-                Console.WriteLine("Se eliminó al jugador" + jugadorActual);
-                ColaTurnos.Dequeue();
-            }
-            Console.WriteLine("Presiona Enter para continuar al siguiente turno...");
-            Turno++;
-
-            if (ColaTurnos.Size == 1)
-            {
-                Console.WriteLine($"Ganó jugador {ColaTurnos.Peek().Nombre}");
-                break;
-            }
-            else if (Turno == 100)
-            {
-                //Se debe evaluar quien tiene más dinero y valor en propiedades
-            }
+            ColaTurnos = InicializarTurnos(jugador1, jugador2, jugador3, jugador4);
+            Turno = 0;
 
             jugadorActual = ColaTurnos.Peek();
 
-            Console.ReadLine(); // Esperar a que el jugador presione Enter antes de continuar
+            Console.WriteLine("El juego ha comenzado. El jugador actual es: " + jugadorActual.Nombre);
+
+            while (true) // Bucle infinito para el juego
+            {
+                AccionCasilla accion = PrepararTurno();
+                Console.WriteLine(accion);
+
+                bool sigueEnJuego = _manejadorAcciones.EjecutarAccion(accion, jugadorActual, Turno);
+                if (sigueEnJuego)
+                {
+                    ColaTurnos.Advance();
+                }
+                else
+                {
+                    Console.WriteLine("Se eliminó al jugador" + jugadorActual);
+                    ColaTurnos.Dequeue();
+                }
+                Console.WriteLine("Presiona Enter para continuar al siguiente turno...");
+                Turno++;
+
+                if (ColaTurnos.Size == 1)
+                {
+                    Console.WriteLine($"Ganó jugador {ColaTurnos.Peek().Nombre}");
+                    break;
+                }
+                else if (Turno == 100)
+                {
+                    //Se debe evaluar quien tiene más dinero y valor en propiedades
+                }
+
+                jugadorActual = ColaTurnos.Peek();
+
+                Console.ReadLine(); // Esperar a que el jugador presione Enter antes de continuar
+            }
+
+
         }
-
-
     }
 }
